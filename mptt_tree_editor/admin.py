e0674ba60e83a9c2164919d0eedd5b8c9c8b19ec
Alex Zayets <alex.consp@gmail.com>
2018-10-16 14:20:09 +0300

    Task-15025, fix 500 on redmine issues page

--- mptt_tree_editor/admin.py
+++ mptt_tree_editor/admin.py
@@ -86,7 +86,7 @@ def _build_tree_structure(cls):
         all_nodes[p_id] = []
 
         if parent_id:
-            if not all_nodes.has_key(parent_id):
+            if parent_id not in all_nodes:
                 # This happens very rarely, but protect against parents that
                 # we have yet to iteratove over.
                 all_nodes[parent_id] = []
